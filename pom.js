50e19427b7d4758ebd621f7d15f9e5485bafa981
Adam Van Ymeren <adam@vany.ca>
2025-08-06 10:30:13 -0700

    clean up pom

--- pom.js
+++ pom.js
@@ -1,20 +1,9 @@
 foam.POM({
   name: 'foamchat',
-  excludes: [ '*' ],
+  version: '0.0.1',
   projects: [
     { name: 'foam3/pom'},
     { name: 'src/com/foamdev/chat/pom'},
     { name: 'journals/pom' }
-  ],
-  licenses: `
-    // Add your license header here
-  `,
-  envs: {
-    VERSION: '0.0.1',
-  },
-  tasks: [
-    function javaManifest() {
-      JAVA_MANIFEST_VENDOR_ID = 'com.foamdev.chat';
-    }
   ]
 });
